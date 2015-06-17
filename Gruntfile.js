5d1319886d24929cfa9cddd35dc76b2190d60a74
Piercus <piercus@gmail.com>
2015-06-18 01:10:04 +0200

    add digikamExport task

--- Gruntfile.js
+++ Gruntfile.js
@@ -444,6 +444,10 @@ module.exports = function (grunt) {
           }
         }
       }
+    },
+
+    exportDigikam : {
+      folder: "<%= yeoman.app %>/data" 
     }
   });
 
@@ -505,4 +509,92 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+
+  grunt.registerTask('exportDigikam', 'export the digikam photo data', function(dbFile){
+    if (arguments.length === 0) {
+      grunt.log.writeln(this.name + ", no args");
+    } else {
+      grunt.log.writeln(this.name + ", " + dbFile);
+      var done = this.async();
+      grunt.config.requires('exportDigikam.folder');
+      var folder= grunt.config('exportDigikam.folder');
+
+      var sqlite3 = require('sqlite3').verbose();
+      console.log(dbFile);
+      var db = new sqlite3.Database(dbFile);
+      var fs = require("fs");
+
+      var writeStreamTags = fs.createWriteStream(folder+"/tags.json",{ 
+        flags: 'w',
+        encoding: "utf8"
+      });
+
+      var writeStreamImgs = fs.createWriteStream(folder+"/images.json",{ 
+        flags: 'w',
+        encoding: "utf8"
+      });
+
+      var writeStreamImgTags = fs.createWriteStream(folder+"/imagesTags.json",{ 
+        flags: 'w',
+        encoding: "utf8"
+      });
+
+      console.log("here");
+      var listTag = [], listImg = [], listImgTags = [];
+
+      var nAsync = 3;
+
+      var closeStream = function(){
+        if(nAsync === 0){
+          db.close();
+          done();
+        }
+      };
+
+      db.serialize(function() {
+        writeStreamTags.write("{\n");
+        db.each("SELECT t.id, t.name, t.pid, GROUP_CONCAT(img.imageid) AS imageIds FROM Tags t JOIN ImageTags img ON t.id = img.tagid GROUP BY t.id;", function(err, row) {
+            listTag.push('  "'+row.id + '": { "name": "'+row.name+'", "pid": '+row.pid+', "imageIds": ['+row.imageIds+']}');
+        }, function(){
+          writeStreamTags.write(listTag.join(",\n")+'\n}');
+          writeStreamTags.close();  
+          nAsync--;
+          closeStream();
+        });
+
+        writeStreamImgs.write("{\n");
+        db.each("SELECT i.id, a.relativePath||'/'||i.name AS src , GROUP_CONCAT(it.tagid) AS tagIds FROM Images i,Albums a, ImageTags it WHERE a.id = i.album AND it.imageid = i.id GROUP BY i.id", function(err, row) {
+            listImg.push('  "'+row.id + '": { "src": "'+row.src+'", "tagIds": ['+row.tagIds+']}');
+        }, function(){
+          writeStreamImgs.write(listImg.join(",\n")+'\n}');
+          writeStreamImgs.close();  
+          nAsync--;
+          closeStream();
+        });
+
+
+        writeStreamImgTags.write("{\n");
+        db.each(
+          //SELECT idSELECT tagid, imageid, property, count(*) FROM ImageTagProperties GROUP BY tagid, imageid, property
+          //SELECT * FROM (SELECT tagid, imageid, property, count(*) as cnt FROM ImageTagProperties GROUP BY tagid, imageid, property) as test where test.cnt > 1
+        'SELECT * FROM ImageTagProperties', function(err, row) {
+            var x = row.value.match(/x=\"([^\"]*)\"/)[1],
+                y = row.value.match(/y=\"([^\"]*)\"/)[1],
+                height = row.value.match(/height=\"([^\"]*)\"/)[1],
+                width = row.value.match(/width=\"([^\"]*)\"/)[1];
+
+            listImgTags.push('  "'+row.imageid+","+row.tagid + '": { "rect": { "x": '+x+', "y": '+y+', "width": '+width+', "height": '+height+'}}');
+        }, function(){
+          writeStreamImgTags.write(listImgTags.join(",\n")+'\n}');
+          writeStreamImgTags.close();  
+          nAsync--;
+          closeStream();
+        });
+
+      });
+
+
+
+    }
+  });
 };
--- README.md
+++ README.md
@@ -9,6 +9,12 @@ Images are coming from a digikam project.
 
 Run `grunt` for building and `grunt serve` for preview.
 
+## export digikam tags
+
+Run
+
+    grunt exportDigikam:<digikam sqlite db file>
+
 ## Testing
 
 Running `grunt test` will run the unit tests.
